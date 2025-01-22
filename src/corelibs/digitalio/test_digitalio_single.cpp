5a43d84601ac8d34942140928c43950041df600d
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2025-01-22 10:33:49 +0530

    src/test_digitalio_single.cpp: Remove pullup pulldown tests due to pin shortage.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

--- src/corelibs/digitalio/test_digitalio_single.cpp
+++ src/corelibs/digitalio/test_digitalio_single.cpp
@@ -2,7 +2,7 @@
  *
  * This test is used to verify the functionality of the Digital IO module.
  * only one board is needed with TEST_DIGITALIO_OUTPUT pin connected to 
- * TEST_DIGITALIO_INPUT_1 pin for the test cases to work as expected.
+ * TEST_DIGITALIO_INPUT pin for the test cases to work as expected.
  */
 
 // std includes
@@ -40,10 +40,7 @@ TEST_GROUP(digitalio_single_internal);
 /**
  * @brief Setup method called by Unity before every test in this test group.
  */
-static TEST_SETUP(digitalio_single_internal)
-{ 
-    pinMode(TEST_DIGITALIO_OUTPUT, OUTPUT);
-    pinMode(TEST_DIGITALIO_INPUT_1, INPUT);
+static TEST_SETUP(digitalio_single_internal) { 
 }
 
 /**
@@ -55,93 +52,71 @@ static TEST_TEAR_DOWN(digitalio_single_internal) {
 /**
  * @brief This test is to verify digitalWrite and digitalRead functions when input pin is set to INPUT mode.
  * 
- * @note: Assumption is made that TEST_DIGITALIO_OUTPUT is connected to TEST_DIGITALIO_INPUT_1
+ * @note: Assumption is made that TEST_DIGITALIO_OUTPUT is connected to TEST_DIGITALIO_INPUT
  *       for the test cases to work as expected.
  */
 TEST_IFX(digitalio_single_internal, test_digitalio_read_write_input_normal)
 {
+    pinMode(TEST_DIGITALIO_OUTPUT, OUTPUT);
+    pinMode(TEST_DIGITALIO_INPUT, INPUT);
+
     digitalWrite(TEST_DIGITALIO_OUTPUT, HIGH);
-    TEST_ASSERT_EQUAL_MESSAGE(HIGH, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should be set to HIGH");
+    TEST_ASSERT_EQUAL_MESSAGE(HIGH, digitalRead(TEST_DIGITALIO_INPUT), "Input Pin should be set to HIGH");
     digitalWrite(TEST_DIGITALIO_OUTPUT, LOW);
-    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should be set to LOW");
+    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT), "Input Pin should be set to LOW");
 }
 
 /**
  * @brief This test is to verify digitalWrite and digitalRead functions when input pin is set to INPUT_PULLUP mode.
  * 
- * @note: Assumption is made that TEST_DIGITALIO_OUTPUT is connected to TEST_DIGITALIO_INPUT_1
+ * @note: Assumption is made that TEST_DIGITALIO_OUTPUT is connected to TEST_DIGITALIO_INPUT
  *       for the test cases to work as expected.
  */
 TEST_IFX(digitalio_single_internal, test_digitalio_read_write_input_pullup)
 {
+    pinMode(TEST_DIGITALIO_OUTPUT, OUTPUT);
+    pinMode(TEST_DIGITALIO_INPUT, INPUT_PULLUP);
+
     digitalWrite(TEST_DIGITALIO_OUTPUT, HIGH);
-    TEST_ASSERT_EQUAL_MESSAGE(HIGH, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should be set to HIGH");
+    TEST_ASSERT_EQUAL_MESSAGE(HIGH, digitalRead(TEST_DIGITALIO_INPUT), "Input Pin should be set to HIGH");
     digitalWrite(TEST_DIGITALIO_OUTPUT, LOW);
-    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should be set to LOW");
+    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT), "Input Pin should be set to LOW");
 }
 
 /**
  * @brief This test is to verify digitalWrite and digitalRead functions when input pin is set to INPUT_PULLDOWN mode.
  * 
- * @note: Assumption is made that TEST_DIGITALIO_OUTPUT is connected to TEST_DIGITALIO_INPUT_1
+ * @note: Assumption is made that TEST_DIGITALIO_OUTPUT is connected to TEST_DIGITALIO_INPUT
  *       for the test cases to work as expected.
  */
 TEST_IFX(digitalio_single_internal, test_digitalio_read_write_input_pulldown) 
 {
+    pinMode(TEST_DIGITALIO_OUTPUT, OUTPUT);
+    pinMode(TEST_DIGITALIO_INPUT, INPUT_PULLDOWN);
+
     digitalWrite(TEST_DIGITALIO_OUTPUT, HIGH);
-    TEST_ASSERT_EQUAL_MESSAGE(HIGH, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should be set to HIGH");
+    TEST_ASSERT_EQUAL_MESSAGE(HIGH, digitalRead(TEST_DIGITALIO_INPUT), "Input Pin should be set to HIGH");
     digitalWrite(TEST_DIGITALIO_OUTPUT, LOW);
-    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should be set to LOW");
+    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT), "Input Pin should be set to LOW");
 }
 
 /**
  * @brief This test is to verify digitalWrite and digitalRead functions when output pin is set to OPENDRAIN mode.
  * 
- * @note: Assumption is made that TEST_DIGITALIO_OUTPUT is connected to TEST_DIGITALIO_INPUT_1
+ * @note: Assumption is made that TEST_DIGITALIO_OUTPUT is connected to TEST_DIGITALIO_INPUT
  *       for the test cases to work as expected.
  */
 TEST_IFX(digitalio_single_internal, test_digitalio_read_write_output_opendrain)
 {
     pinMode(TEST_DIGITALIO_OUTPUT, OUTPUT_OPENDRAIN);
-    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should be set to LOW initially");
+    pinMode(TEST_DIGITALIO_INPUT, INPUT);
+
+    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT), "Input Pin should be set to LOW initially");
 
     digitalWrite(TEST_DIGITALIO_OUTPUT, HIGH);
-    TEST_ASSERT_EQUAL_MESSAGE(HIGH, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should be set to HIGH");
+    TEST_ASSERT_EQUAL_MESSAGE(HIGH, digitalRead(TEST_DIGITALIO_INPUT), "Input Pin should be set to HIGH");
     digitalWrite(TEST_DIGITALIO_OUTPUT, LOW);
-    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should be set to LOW");
-}
-
-/**
- * @brief This test is to verify input pin is set to INPUT mode correctly.
- * 
- * @note: Assumption is made that TEST_DIGITALIO_INPUT_2 is not connected to any other pin
- */
-TEST_IFX(digitalio_single_internal, test_digitalio_input_mode_default)
-{
-    pinMode(TEST_DIGITALIO_INPUT_2, INPUT);
-    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should read LOW initially");  
-}
-
-/**
- * @brief This test is to verify input pin is set to INPUT_PULLUP mode correctly.
- * 
- * @note: Assumption is made that TEST_DIGITALIO_INPUT_2 is not connected to any other pin
- */
-TEST_IFX(digitalio_single_internal, test_digitalio_input_mode_pullup)
-{
-    pinMode(TEST_DIGITALIO_INPUT_2, INPUT_PULLUP);
-    TEST_ASSERT_EQUAL_MESSAGE(HIGH, digitalRead(TEST_DIGITALIO_INPUT_2), "Input Pin should read HIGH initially due to pull-up"); 
-}
-
-/**
- * @brief This test is to verify input pin is set to INPUT_PULLDOWN mode correctly.
- * 
- * @note: Assumption is made that TEST_DIGITALIO_INPUT_2 is not connected to any other pin
- */
-TEST_IFX(digitalio_single_internal, test_digitalio_input_mode_pulldown)
-{
-    pinMode(TEST_DIGITALIO_INPUT_2, INPUT_PULLDOWN);
-    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT_1), "Input Pin should read LOW initially due to pull-down"); 
+    TEST_ASSERT_EQUAL_MESSAGE(LOW, digitalRead(TEST_DIGITALIO_INPUT), "Input Pin should be set to LOW");
 }
 
 /**
@@ -171,7 +146,7 @@ TEST_IFX(digitalio_single_internal, test_digitalRead_invalid_pin) {
  * @brief Test invalid pin mode
 */
 TEST_IFX(digitalio_single_internal, test_invalid_pinmode) {
-    pinMode(TEST_DIGITALIO_INPUT_2, 255);
+    pinMode(TEST_DIGITALIO_INPUT, 255);
     // No assertion as pinMode doesn't return a value, but ensure no crash
 }
 
@@ -184,9 +159,6 @@ static TEST_GROUP_RUNNER(digitalio_single_internal)
     RUN_TEST_CASE(digitalio_single_internal, test_digitalio_read_write_input_pullup);
     RUN_TEST_CASE(digitalio_single_internal, test_digitalio_read_write_input_pulldown);
     RUN_TEST_CASE(digitalio_single_internal, test_digitalio_read_write_output_opendrain);
-    RUN_TEST_CASE(digitalio_single_internal, test_digitalio_input_mode_default);
-    RUN_TEST_CASE(digitalio_single_internal, test_digitalio_input_mode_pullup);
-    RUN_TEST_CASE(digitalio_single_internal, test_digitalio_input_mode_pulldown);
     RUN_TEST_CASE(digitalio_single_internal, test_pinMode_invalid_pin);
     RUN_TEST_CASE(digitalio_single_internal, test_digitalWrite_invalid_pin);
     RUN_TEST_CASE(digitalio_single_internal, test_digitalRead_invalid_pin);
