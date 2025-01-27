b4396b8c9303114653e80cea93df9003488d8cf9
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-01-27 16:57:55 +0100

    src/corelibs/wifi: Added status and localIP basic tests.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- src/corelibs/wifi/test_wifi_ap.cpp
+++ src/corelibs/wifi/test_wifi_ap.cpp
@@ -8,19 +8,38 @@ static TEST_SETUP(wifi_ap) {
 }
 
 static TEST_TEAR_DOWN(wifi_ap) {
-    WiFi.end();
 }
 
-TEST(wifi_ap, begin_ap) {
+TEST_IFX(wifi_ap, begin_ap) {
     int result = WiFi.beginAP("arduino-wifi-ap", "wifi-ap-password", 1);
     TEST_ASSERT_EQUAL_INT(WL_AP_CONNECTED, result);
+}
 
-    /* Wait forever for now. */
-    while(true) {};
+TEST_IFX(wifi_ap, is_status_connected) {
+    TEST_ASSERT_EQUAL_INT(WL_AP_CONNECTED, WiFi.status());
+}
+
+TEST_IFX(wifi_ap, check_local_ip) {
+    IPAddress ip = WiFi.localIP();
+    TEST_ASSERT_EQUAL_INT(192, ip[0]);
+    TEST_ASSERT_EQUAL_INT(168, ip[1]);
+    TEST_ASSERT_EQUAL_INT(0, ip[2]);
+    TEST_ASSERT_EQUAL_INT(1, ip[3]);
+}
+
+TEST_IFX(wifi_ap, wifi_end) {
+    WiFi.end();
 }
 
 TEST_GROUP_RUNNER(wifi_ap) {
     RUN_TEST_CASE(wifi_ap, begin_ap);
+    RUN_TEST_CASE(wifi_ap, is_status_connected);
+    RUN_TEST_CASE(wifi_ap, check_local_ip);
+    /* Wait forever for now. */
+    /* This allows to check the sta
+    test manually. */
+    while(true) {};
+    RUN_TEST_CASE(wifi_ap, wifi_end);
 }
 
 
--- src/corelibs/wifi/test_wifi_sta.cpp
+++ src/corelibs/wifi/test_wifi_sta.cpp
@@ -8,10 +8,9 @@ static TEST_SETUP(wifi_sta) {
 }
 
 static TEST_TEAR_DOWN(wifi_sta) {
-    WiFi.end();
 }
 
-TEST(wifi_sta, connect_to_ap) {
+TEST_IFX(wifi_sta, connect_to_ap) {
     /* This AP is created by the test_wifi_ap.
        Currently the test tools does provide a way to and synch multitest. 
        It has only be validated manually. */
@@ -19,8 +18,27 @@ TEST(wifi_sta, connect_to_ap) {
     TEST_ASSERT_EQUAL_INT(WL_CONNECTED, result);
 }
 
+TEST_IFX(wifi_sta, is_status_connected) {
+    TEST_ASSERT_EQUAL_INT(WL_CONNECTED, WiFi.status());
+}
+
+TEST_IFX(wifi_sta, check_local_ip) {
+    IPAddress ip = WiFi.localIP();
+    TEST_ASSERT_EQUAL_INT(192, ip[0]);
+    TEST_ASSERT_EQUAL_INT(168, ip[1]);
+    TEST_ASSERT_EQUAL_INT(0, ip[2]);
+    TEST_ASSERT_EQUAL_INT(2, ip[3]);
+}
+
+TEST_IFX(wifi_sta, wifi_end) {
+    WiFi.end();
+}
+
 TEST_GROUP_RUNNER(wifi_sta) {
     RUN_TEST_CASE(wifi_sta, connect_to_ap);
+    RUN_TEST_CASE(wifi_sta, is_status_connected);
+    RUN_TEST_CASE(wifi_sta, check_local_ip);  
+    RUN_TEST_CASE(wifi_sta, wifi_end);  
 }
 
 
