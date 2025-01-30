00bb8c95f6548c8979b943da8a110f7e60e4eabc
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-01-30 15:02:22 +0100

    src/corelibs/wifi: Added basic setup for client server tests.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- src/corelibs/wifi/test_wifi_client.cpp
+++ src/corelibs/wifi/test_wifi_client.cpp
@@ -11,13 +11,27 @@ static TEST_SETUP(wifi_client) {
 static TEST_TEAR_DOWN(wifi_client) {
 }
 
-TEST_IFX(wifi_client, begin_ap) {
+TEST_IFX(wifi_client, wifi_connect_to_ap) {
+   /* This AP is created by the test_wifi_ap.
+    Currently the test tools does provide a way to and synch multitest. 
+    It has only be validated manually. */
+    int result = WiFi.begin("arduino-wifi-ap", "wifi-ap-password");
+    TEST_ASSERT_EQUAL_INT(WL_CONNECTED, result);
+}
+
+TEST_IFX(wifi_client, client_begin) {
+}
+
+TEST_IFX(wifi_client, client_connect) {
 }
 
 TEST_IFX(wifi_client, wifi_end) {
+    WiFi.end();
 }
 
 TEST_GROUP_RUNNER(wifi_client) {
-    RUN_TEST_CASE(wifi_client, begin_ap);
+    RUN_TEST_CASE(wifi_client, wifi_connect_to_ap);
+    RUN_TEST_CASE(wifi_client, client_begin);
+    RUN_TEST_CASE(wifi_client, client_connect);
     RUN_TEST_CASE(wifi_client, wifi_end);
 }
--- src/corelibs/wifi/test_wifi_server.cpp
+++ src/corelibs/wifi/test_wifi_server.cpp
@@ -11,21 +11,34 @@ static TEST_SETUP(wifi_server) {
 static TEST_TEAR_DOWN(wifi_server) {
 }
 
-TEST_IFX(wifi_server, begin_ap) {
+
+TEST_IFX(wifi_server, wifi_begin_ap) {
+    int result = WiFi.beginAP("arduino-wifi-ap", "wifi-ap-password", 1);
+    TEST_ASSERT_EQUAL_INT(WL_AP_CONNECTED, result);
 }
 
-TEST_IFX(wifi_server, is_status_connected) {
+WiFiServer server;
+
+TEST_IFX(wifi_server, server_begin) {
+    server.begin(80);   
 }
 
-TEST_IFX(wifi_server, check_local_ip) {
+TEST_IFX(wifi_server, server_end) {
+    server.end();
 }
 
 TEST_IFX(wifi_server, wifi_end) {
+    WiFi.end();
 }
 
 TEST_GROUP_RUNNER(wifi_server) {
-    RUN_TEST_CASE(wifi_server, begin_ap);
-    RUN_TEST_CASE(wifi_server, is_status_connected);
-    RUN_TEST_CASE(wifi_server, check_local_ip);
+    RUN_TEST_CASE(wifi_server, wifi_begin_ap);
+    RUN_TEST_CASE(wifi_server, server_begin);
+    /* Wait forever for now. */
+    /* This allows to check the sta
+    test manually. */
+    while(true) {};
+    RUN_TEST_CASE(wifi_server, server_end);
     RUN_TEST_CASE(wifi_server, wifi_end);
+
 }
