90dbf32991f1e64d50edd9ea25cd25d4e3ce1396
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2025-04-02 15:56:58 +0530

    Implement SPI master slave ping pong tests.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

---
+++ src/corelibs/spi/test_spi_connected2_masterpingpong.cpp
@@ -0,0 +1,183 @@
+/** 
+ * @name    test_spi_connected2_masterpingpong.cpp
+ *
+ * @brief   This test is used to verify the functionality of the SPI transfer functions.
+ *          2 boards are needed with SPI pins of master board connected to SPI pins of slave board.
+ *          Also a sync pin is used to synchronize the transfer between master and slave.
+ * 
+ * @note    These tests are expected to be executed after slave tests are executed.
+ *          Make sure no SPI data is being transferred before executing these tests which
+ *          may interfere with the test results.
+ **/
+
+// std includes
+
+// test includes
+#include "test_common_includes.h"
+
+// project includes
+#include <SPI.h>
+
+const uint8_t MAX_BUFFER_SIZE = 20;
+const uint8_t MAX_TEST_ITERATION = 10;
+
+// variables
+static SPIClassPSOC *spi_master = nullptr;
+static uint8_t testTransmitBuff[MAX_BUFFER_SIZE+1] = {0};
+static uint8_t expectedReceiveBuff[MAX_BUFFER_SIZE] = {0};
+
+
+// Method invoked before a test suite is run.
+static void spi_connected2_masterpingpong_suite_setup() {
+    pinMode(TEST_SPI_SLAVE_SELECT, OUTPUT);
+    pinMode(TEST_SYNC_INPUT_OUTPUT, OUTPUT);
+    spi_master = &SPI;
+
+    digitalWrite(TEST_SPI_SLAVE_SELECT, HIGH);
+    digitalWrite(TEST_SYNC_INPUT_OUTPUT, LOW);
+    spi_master->begin();
+}
+
+// Method invoked after a test suite is run.
+static void spi_connected2_masterpingpong_suite_teardown() {
+    digitalWrite(TEST_SPI_SLAVE_SELECT, HIGH);
+    digitalWrite(TEST_SYNC_INPUT_OUTPUT, LOW);
+    spi_master->end();
+}
+
+// Test group definition
+TEST_GROUP(spi_connected2_masterpingpong);
+
+TEST_SETUP(spi_connected2_masterpingpong) {
+
+}
+TEST_TEAR_DOWN(spi_connected2_masterpingpong) {
+    
+}
+
+
+/**
+ * @brief    This test transfers bytes in a loop specified by MAX_TEST_ITERATION from master to slave.
+ * 
+ * @details  The first byte received from slave is dummy and ignored.
+ *           From the second byte onwards, the slave increments its previously
+ *           transmitted byte from slave and sends it to the master.
+ *           The master increments the previously transmitted byte from master
+ *           and sends it to the slave.
+ * 
+ * @note     The sync pin is pulled high before the transfer starts and pulled low after the transfer ends.
+ *           The test will stop when the sync pin is pulled low.
+ */
+TEST_IFX(spi_connected2_masterpingpong, test_ping_pong_transfer_byte) {
+
+    uint8_t testTransmitByte = 0xAA;
+    uint8_t testReceiveByte = 0xCC;
+    uint8_t expectedReceiveByte = testReceiveByte;
+
+    digitalWrite(TEST_SYNC_INPUT_OUTPUT, HIGH);
+
+    digitalWrite(TEST_SPI_SLAVE_SELECT, LOW);
+    testReceiveByte =  spi_master->transfer(testTransmitByte); // first byte received is dummy and ignored
+    digitalWrite(TEST_SPI_SLAVE_SELECT, HIGH);
+
+    for (uint8_t i = 1; i < MAX_TEST_ITERATION; i++) {   
+        testTransmitByte++;
+        
+        digitalWrite(TEST_SPI_SLAVE_SELECT, LOW);
+        testReceiveByte = spi_master->transfer(testTransmitByte);
+        digitalWrite(TEST_SPI_SLAVE_SELECT, HIGH);
+
+        TEST_ASSERT_EQUAL_UINT8_MESSAGE(expectedReceiveByte, testReceiveByte, "SPI Master PingPong transfer byte failed");
+
+        expectedReceiveByte = testReceiveByte + 1; // slave increments the previously transmitted byte from slave
+    }
+
+    digitalWrite(TEST_SYNC_INPUT_OUTPUT, LOW);
+}
+
+
+/**
+ * @brief    This test transfers words in a loop specified by MAX_TEST_ITERATION from master to slave.
+ * 
+ * @details  The first byte received from slave is dummy and ignored.
+ *           From the second word onwards, the slave increments its previously
+ *           transmitted word from slave and sends it to the master.
+ *           The master increments the previously transmitted word from master
+ *           and sends it to the slave.
+ * 
+ * @note     The sync pin is pulled high before the transfer starts and pulled low after the transfer ends.
+ *           The test will stop when the sync pin is pulled low.
+ */
+TEST_IFX(spi_connected2_masterpingpong, test_ping_pong_transfer_word) {
+
+    uint16_t testTransmitWord = 0x4433;
+    uint16_t testReceiveWord = 0x5522; // first word read from slave
+    uint16_t expectedReceiveWord = testReceiveWord;
+    uint16_t result = 0;
+
+    digitalWrite(TEST_SYNC_INPUT_OUTPUT, HIGH);
+
+    digitalWrite(TEST_SPI_SLAVE_SELECT, LOW);
+    testReceiveWord =  spi_master->transfer16(testTransmitWord); // first byte received is dummy and ignored
+    digitalWrite(TEST_SPI_SLAVE_SELECT, HIGH);
+
+
+    for (uint8_t i = 1; i < MAX_TEST_ITERATION; i++) {
+        testTransmitWord++;
+        result = testReceiveWord << 8;
+
+        digitalWrite(TEST_SPI_SLAVE_SELECT, LOW);
+        testReceiveWord = spi_master->transfer16(testTransmitWord);
+        digitalWrite(TEST_SPI_SLAVE_SELECT, HIGH);
+
+        result = result | testReceiveWord >> 8; // combine the two bytes to form a 16-bit word
+
+        TEST_ASSERT_EQUAL_UINT16_MESSAGE(expectedReceiveWord, result, "SPI Master PingPong transfer word failed");
+
+        expectedReceiveWord = result + 1; // slave increments the previously transmitted word from slave
+    }
+
+    digitalWrite(TEST_SYNC_INPUT_OUTPUT, LOW);
+}
+
+
+/**
+ * @brief    This test transfers buffer of data specified by MAX_BUFFER_SIZE from master to slave.
+ * 
+ * @details  The first byte received from slave is dummy and ignored.
+ *           From the second byte onwards, the data received from the slave is valid.
+ *           In order to receive all the data from slave, the master sends MAX_BUFFER_SIZE+1 bytes to the slave.
+ * 
+ * @note     The sync pin is pulled high before the transfer starts and pulled low after the transfer ends.
+ *           The test will stop when the sync pin is pulled low.
+ */
+TEST_IFX(spi_connected2_masterpingpong, test_ping_pong_transfer_buffer) {
+
+    for (uint8_t i = 0; i < MAX_BUFFER_SIZE; i++) {
+        testTransmitBuff[i] = 0xAA + i;
+        expectedReceiveBuff[i] = 0xBB + i;
+    }
+
+    digitalWrite(TEST_SYNC_INPUT_OUTPUT, HIGH);
+
+    digitalWrite(TEST_SPI_SLAVE_SELECT, LOW);
+    spi_master->transfer(testTransmitBuff, MAX_BUFFER_SIZE+1); 
+    digitalWrite(TEST_SPI_SLAVE_SELECT, HIGH);
+
+    digitalWrite(TEST_SYNC_INPUT_OUTPUT, LOW);
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expectedReceiveBuff, testTransmitBuff+1, MAX_BUFFER_SIZE, "SPI Master PingPong transfer word failed");
+}
+
+
+// Bundle all tests to be executed for this test group
+TEST_GROUP_RUNNER(spi_connected2_masterpingpong) {
+    spi_connected2_masterpingpong_suite_setup();
+
+    RUN_TEST_CASE(spi_connected2_masterpingpong, test_ping_pong_transfer_byte);
+    RUN_TEST_CASE(spi_connected2_masterpingpong, test_ping_pong_transfer_word);
+    RUN_TEST_CASE(spi_connected2_masterpingpong, test_ping_pong_transfer_buffer);
+
+    spi_connected2_masterpingpong_suite_teardown();
+
+}
---
+++ src/corelibs/spi/test_spi_connected2_slavepingpong.cpp
@@ -0,0 +1,137 @@
+/** 
+ * @name    test_spi_connected2_slavepingpong.cpp
+ *
+ * @brief   This test is used to verify the functionality of the SPI transfer functions.
+ *          2 boards are needed with SPI pins of master board connected to SPI pins of slave board.
+ *          Also a sync pin is used to synchronize the transfer between master and slave.
+ * 
+ * @note    These tests are expected to be executed after slave tests are executed.
+ *          Make sure no SPI data is being transferred before executing these tests which
+ *          may interfere with the test results.
+ **/
+
+// std includes
+
+// test includes
+#include "test_common_includes.h"
+#include "pins_arduino.h"
+
+// project includes
+#include <SPI.h>
+
+const uint8_t MAX_BUFFER_SIZE = 20;
+const uint8_t MAX_TEST_ITERATION = 10;
+// variables
+
+static uint8_t testTransmitBuff[MAX_BUFFER_SIZE] = {0};
+static uint8_t expectedReceiveBuff[MAX_BUFFER_SIZE] = {0};
+static SPIClassPSOC SPI1 = SPIClassPSOC(PIN_SPI_MOSI, PIN_SPI_MISO, PIN_SPI_SCK, TEST_SPI_SLAVE_SELECT, true);
+static SPIClassPSOC *spi_slave = &SPI1;
+
+// Method invoked before a test suite is run.
+static void spi_connected2_slavepingpong_suite_setup() {
+    pinMode(TEST_SYNC_INPUT_OUTPUT, INPUT_PULLDOWN);
+    spi_slave->begin();
+}
+
+// Method invoked after a test suite is run.
+static void spi_connected2_slavepingpong_suite_teardown() {
+    spi_slave->end();
+}
+
+// Test group definition
+TEST_GROUP(spi_connected2_slavepingpong);
+
+TEST_SETUP(spi_connected2_slavepingpong) {
+}
+
+TEST_TEAR_DOWN(spi_connected2_slavepingpong) {
+}
+
+
+/**
+ * @brief    This test transfers bytes in a loop specified by MAX_TEST_ITERATION to the master.
+ * 
+ * @details  Slave increments its previously transmitted byte and sends it to the master.
+ *           The master increments the previously transmitted byte from master
+ *           and sends it to the slave.
+ * 
+ * @note     Slave initailly waits for the master to pull the sync pin high.
+ */
+TEST_IFX(spi_connected2_slavepingpong, test_ping_pong_transfer_byte) {
+    uint8_t testTranceiveByte = 0xCC;
+    uint8_t testReceiveByte = 0xAA;
+    uint8_t expectedReceiveByte = testReceiveByte;
+    
+    while (digitalRead(TEST_SYNC_INPUT_OUTPUT) == LOW) {
+        // Wait for the master to pull the sync pin high
+    }
+
+    for (uint8_t i = 1; i < MAX_TEST_ITERATION + 1; i++) {
+        testReceiveByte = spi_slave->transfer(testTranceiveByte);
+        testTranceiveByte++;
+
+        TEST_ASSERT_EQUAL_UINT8_MESSAGE(expectedReceiveByte, testReceiveByte, "SPI Slave PingPong byte transfer failed");
+
+        expectedReceiveByte = testReceiveByte + 1; // master increments the previously transmitted byte from master
+    }
+}
+
+/**
+ * @brief    This test transfers words in a loop specified by MAX_TEST_ITERATION to the master.
+ * 
+ * @details  Slave increments its previously transmitted word and sends it to the master.
+ *           The master increments the previously transmitted word from master
+ *           and sends it to the slave.
+ * 
+ * @note     Slave initailly waits for the master to pull the sync pin high.
+ */
+TEST_IFX(spi_connected2_slavepingpong, test_ping_pong_transfer_word) {
+    uint16_t testTranceiveWord = 0x5522;
+    uint16_t testReceiveWord = 0x4433; // first word read from master
+    uint16_t expectedReceiveWord = testReceiveWord;
+    
+    while (digitalRead(TEST_SYNC_INPUT_OUTPUT) == LOW) {
+        // Wait for the master to pull the sync pin high
+    }
+
+    for (uint8_t i = 1; i < MAX_TEST_ITERATION + 1; i++) {
+        testReceiveWord = spi_slave->transfer16(testTranceiveWord);
+        testTranceiveWord++;
+
+        TEST_ASSERT_EQUAL_UINT16_MESSAGE(expectedReceiveWord, testReceiveWord, "SPI Slave PingPong word transfer failed");
+
+        expectedReceiveWord = testReceiveWord + 1; // master increments the previously transmitted word from master
+    }
+}
+
+/**
+ * @brief    This test transfers a buffer of data specified by MAX_BUFFER_SIZE to the master.
+ * 
+ * @note     Slave initailly waits for the master to pull the sync pin high.
+ */
+TEST_IFX(spi_connected2_slavepingpong, test_ping_pong_transfer_buffer) {
+    for (uint8_t i = 0; i < MAX_BUFFER_SIZE; i++) {
+        testTransmitBuff[i] = 0xBB + i;
+        expectedReceiveBuff[i] = 0xAA + i;
+    }
+
+    while (digitalRead(TEST_SYNC_INPUT_OUTPUT) == LOW) {
+        // Wait for the master to pull the sync pin high
+    }
+
+    spi_slave->transfer(testTransmitBuff, MAX_BUFFER_SIZE);
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expectedReceiveBuff, testTransmitBuff, MAX_BUFFER_SIZE, "SPI Master PingPong transfer buffer failed");
+}
+
+// Bundle all tests to be executed for this test group
+TEST_GROUP_RUNNER(spi_connected2_slavepingpong) {
+    spi_connected2_slavepingpong_suite_setup();
+
+    RUN_TEST_CASE(spi_connected2_slavepingpong, test_ping_pong_transfer_byte);
+    RUN_TEST_CASE(spi_connected2_slavepingpong, test_ping_pong_transfer_word);
+    RUN_TEST_CASE(spi_connected2_slavepingpong, test_ping_pong_transfer_buffer);
+
+    spi_connected2_slavepingpong_suite_teardown();
+}
