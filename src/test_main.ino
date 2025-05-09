6aed11783949a3f6f1b3c2db5f0c1960ddf55461
Frederikwag <frederik.wagner@infineon.com>
2025-05-09 18:40:01 +0200

    src: Updated test_main.ino.
    Signed-off-by: Frederikwag <frederik.wagner@infineon.com>

--- src/test_main.ino
+++ src/test_main.ino
@@ -22,6 +22,18 @@ void RunAllTests(void)
 
 #endif
 
+#ifdef TEST_ANALOGIO_ADC
+
+    RUN_TEST_GROUP(analogio_adc);
+
+#endif
+
+#ifdef TEST_ANALOGIO_PWM
+
+    RUN_TEST_GROUP(analogio_pwm);
+
+#endif
+
 #ifdef TEST_INTERRUPTS_SINGLE
 
     RUN_TEST_GROUP(gpio_interrupts_single);
@@ -38,6 +50,18 @@ void RunAllTests(void)
 #ifdef TEST_UART_CONNECTED2_RX
 
     RUN_TEST_GROUP(uart_connected2_rx);
+
+#endif
+
+#ifdef TEST_UART_RX
+
+    RUN_TEST_GROUP(uart_rx);
+
+#endif
+
+#ifdef TEST_UART_TX
+
+    RUN_TEST_GROUP(uart_tx);
 
 #endif
 
@@ -117,15 +141,27 @@ void RunAllTests(void)
 
 #endif
 
+#ifdef TEST_WIFI_EXCEPTIONS
+
+    RUN_TEST_GROUP(wifi_exceptions);
+
+#endif
+
 #ifdef TEST_SPI_CONNECTED1_LOOPBACK
 
     RUN_TEST_GROUP(spi_connected1_loopback);
 
 #endif
 
-#ifdef TEST_SPI_3WIRE
+#ifdef TEST_SPI_CONNECTED2_MASTERPINGPONG
 
-    RUN_TEST_GROUP(spi_3wire);
+    RUN_TEST_GROUP(spi_connected2_masterpingpong);
+
+#endif
+
+#ifdef TEST_SPI_CONNECTED2_SLAVEPINGPONG
+
+    RUN_TEST_GROUP(spi_connected2_slavepingpong);
 
 #endif
 
