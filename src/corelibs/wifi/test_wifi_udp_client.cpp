ea91d2d062bc5a9f981f7655b7cdb5dad7e5504c
IFX-Anusha <Anusha.TR@infineon.com>
2025-05-13 12:20:41 +0530

    src/corelibs: Write test.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- src/corelibs/wifi/test_wifi_udp_client.cpp
+++ src/corelibs/wifi/test_wifi_udp_client.cpp
@@ -20,6 +20,24 @@ TEST_IFX(wifi_udp_client, wifi_connect_to_ap) {
     TEST_ASSERT_EQUAL_INT(WL_CONNECTED, result);
 }
 
+TEST_IFX(wifi_udp_client, udp_beginpacket) {
+    IPAddress ip(127, 0, 0, 1);
+    uint16_t port = 80;
+    TEST_ASSERT_TRUE(udpClient.beginPacket(ip, port)); // Start a packet to send
+}
+
+TEST_IFX(wifi_udp_client, udp_write){
+    const char *message = "Hello, UDP!";
+    size_t bytes_written = udpClient.write(message[0]); // Write a single byte
+    TEST_ASSERT_EQUAL_INT(1, bytes_written); // Check if all bytes were written
+
+    bytes_written = udpClient.write((const uint8_t *)message, strlen(message));
+    TEST_ASSERT_EQUAL_INT(strlen(message), bytes_written); // Check if all bytes were written
+}
+
 TEST_GROUP_RUNNER(wifi_udp_client) {
     RUN_TEST_CASE(wifi_udp_client, wifi_connect_to_ap);
+    RUN_TEST_CASE(wifi_udp_client, udp_beginpacket);
+    RUN_TEST_CASE(wifi_udp_client, udp_write);
+    while (true) {}; // Keep the client running
 }
