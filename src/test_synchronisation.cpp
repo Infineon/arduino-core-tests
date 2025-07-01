a4d57069a5408ecda3c1b43a63652a8e60b6b8d5
EPELABPC <epelabpc@epelabpc.blc.infineon.com>
2025-07-01 09:21:33 +0530

    Makefile: ./src: Add synch mechanism between pingpong tests.
    Signed-off-by: EPELABPC <epelabpc@epelabpc.blc.infineon.com>

---
+++ src/test_synchronisation.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void readSerialAndRespond() {
+   String s("");
+
+    do {
+        s = Serial.readString();
+    } while (s.indexOf("[@START_TEST@]") == -1);
+
+    Serial.println(s);
+    Serial.flush();
+}
