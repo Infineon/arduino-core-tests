e968e48d03f70e3a3900dc389ea81aa3ab99a403
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-09-05 14:57:41 +0200

    src/corelibs/wifi/test_wifi_extras.cpp: Enabled host_by_name test.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- src/corelibs/wifi/test_wifi_extras.cpp
+++ src/corelibs/wifi/test_wifi_extras.cpp
@@ -109,8 +109,7 @@ TEST_IFX(wifi_extras, wifi_end) {
 TEST_GROUP_RUNNER(wifi_extras) {
     RUN_TEST_CASE(wifi_extras, wifi_connect_to_ap);
     RUN_TEST_CASE(wifi_extras, client_connect_by_hostname);
-    // TODO: This test needs to fixed as the host name is not resolved with expected ip address
-    // RUN_TEST_CASE(wifi_extras, check_host_by_name);
+    RUN_TEST_CASE(wifi_extras, check_host_by_name);
     RUN_TEST_CASE(wifi_extras, check_dns);
     RUN_TEST_CASE(wifi_extras, check_ping);
     RUN_TEST_CASE(wifi_extras, check_set_dns);
